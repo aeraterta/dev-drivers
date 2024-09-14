68579a21656e6a696b2064f44bb1b3f4c5581d03
aeraterta <aeraterta@gmail.com>
2024-09-14 19:27:35 +0800

    fix missing apps

---
+++ apps/i2c/src/main.c
@@ -0,0 +1,44 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  - Anthony E.Raterta
+ * @version V1.0.0
+ * @date    13-September-2024
+ * @brief   Application entrypoint for I2C testing
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 mcu-dev
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include "resources/hardware/i2c.h"
+
+#define ACC_ADDR 0x19
+#define ACC_REG_ID 0x20
+#define i2c0_master DT_NODELABEL(i2c0)
+
+struct device *i2c0_dev;
+
+int main(void) {
+	if (init_i2c0(&i2c0_dev)) {
+		printk("Initialization successful!\r\n");
+	}
+	else {
+		printk("Initialization failed!\r\n");
+	}
+	uint8_t read_data;
+	int ret = i2c0_read_byte(&i2c0_dev, ACC_ADDR, ACC_REG_ID, &read_data);
+    printk("read_data: 0x%.2x\r\n", read_data);
+	printk("ret: %d\r\n", ret);
+	return 0;
+}
